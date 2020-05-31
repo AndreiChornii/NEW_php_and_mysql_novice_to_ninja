bcbc3b1f0f55f85da64c5751b37d5a0c588b2be1
AndreiChornii <andrei.chornii@gmail.com>
2020-05-31 10:39:57 +0300

    till_p_650_675

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -11,27 +11,27 @@
         <?php foreach ($jokes as $joke): ?>
             <blockquote>
                 <!-- Remove the opening tag <p> -->
-                    <span><?= $joke->rate ?> </span>
-                    <a href="/rate?id=<?= $joke->id ?>"><img src="https://img.icons8.com/material-rounded/24/000000/facebook-like.png"/></a>
-                        <!-- <?=$joke->joketext?>-->
-                        <?=(new \Ninja\Markdown($joke->joketext))->toHtml()?> 
+                <span><?= $joke->rate ?> </span>
+                <a href="/rate?id=<?= $joke->id ?>"><img src="https://img.icons8.com/material-rounded/24/000000/facebook-like.png"/></a>
+                <!-- <?= $joke->joketext ?>-->
+                <?= (new \Ninja\Markdown($joke->joketext))->toHtml() ?> 
 
-                    (by <a href="mailto:<?= htmlspecialchars($joke->getAuthor()->email, ENT_QUOTES, 'UTF-8');
-                    ?>">
-                               <?= htmlspecialchars($joke->getAuthor()->name, ENT_QUOTES, 'UTF-8');
-                               ?></a> on 
-                    <?php
-                    $date = new DateTime($joke->jokedate);
+                (by <a href="mailto:<?= htmlspecialchars($joke->getAuthor()->email, ENT_QUOTES, 'UTF-8');
+                ?>">
+                           <?= htmlspecialchars($joke->getAuthor()->name, ENT_QUOTES, 'UTF-8');
+                           ?></a> on 
+                <?php
+                $date = new DateTime($joke->jokedate);
 
-                    echo $date->format('jS F Y');
-                    ?>)
-                        <?php if ($user): ?>
-                            <?php if ($user->id == $joke->authorid || $user->hasPermission(\Ijdb\Entity\Author::EDIT_JOKES)):
-                                ?>
-                            <a href="/joke/edit?id=<?= $joke->id ?>">Edit</a>
-                        <?php endif; ?>
-                        <?php if ($user->id == $joke->authorid || $user->hasPermission(\Ijdb\Entity\Author::DELETE_JOKES)):
+                echo $date->format('jS F Y');
+                ?>)
+                    <?php if ($user): ?>
+                        <?php if ($user->id == $joke->authorid || $user->hasPermission(\Ijdb\Entity\Author::EDIT_JOKES)):
                             ?>
+                        <a href="/joke/edit?id=<?= $joke->id ?>">Edit</a>
+                    <?php endif; ?>
+                    <?php if ($user->id == $joke->authorid || $user->hasPermission(\Ijdb\Entity\Author::DELETE_JOKES)):
+                        ?>
                         <form action="/joke/delete" method="post">
                             <input type="hidden" name="id" value="<?= $joke->id ?>">
                             <input type="submit" value="Delete">
@@ -41,6 +41,14 @@
                 <!-- Remove the closing tag </p> -->
             </blockquote>
         <?php endforeach; ?>
+        Select page:
+        <?php
+// Calculate the number of pages
+        $numPages = ceil($totalJokes / 10);
+// Display a link for each page
+        for ($i = 1; $i <= $numPages; $i++):
+        ?>
+            <a href="/joke/list?page=<?= $i ?>"><?= $i ?></a>
+    <?php endfor; ?>
     </div>
-
 </div>
